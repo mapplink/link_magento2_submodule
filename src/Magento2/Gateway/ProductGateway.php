d628fa6e1087b61a15374dc4713800024ec56010
gelysis <andreas@gelysis.net>
2017-02-01 12:43:48 +1300

    Added new colour Charcoal Stripe

--- src/Magento2/Gateway/ProductGateway.php
+++ src/Magento2/Gateway/ProductGateway.php
@@ -127,7 +127,7 @@ class ProductGateway extends AbstractGateway
         629=>'Houndstooth', 630=>'Pinstripe Mix', 631=>'Hands', 632=>'Citron', 633=>'Classical', 634=>'Blushing',
         635=>'John x Yoko', 636=>'Charcoal/Putty', 637=>'Charcoal/Yellow', 638=>'Navy/Putty', 639=>'Blue/Charcoal',
         640=>'Joy', 641=>'Nirvana', 642=>'Record', 643=>'Bat Skeleton', 644=>'Love Song', 645=>'Jane', 646=>'Kiss',
-        647=>'Black/Orange');
+        647=>'Black/Orange', 648=>'Charcoal Stripe');
 
     // ToDo: Move mapping to config
     /** @var array self::$sizeById */
