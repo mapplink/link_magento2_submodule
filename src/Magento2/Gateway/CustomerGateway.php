161b7cd439cb82024f417dff435d724eb907effb
gelysis <andreas@gelysis.net>
2017-04-06 12:24:08 +1200

    Added and changed license

--- src/Magento2/Gateway/CustomerGateway.php
+++ src/Magento2/Gateway/CustomerGateway.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Magento2\Gateway\CustomerGateway
+ *
+ * @category Magento2
+ * @package Magento2\Gateway
+ * @author Andreas Gerhards <andreas@lero9.co.nz>
+ * @copyright Copyright (c) 2014 LERO9 Ltd.
+ * @license http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause - Please view LICENSE.md for more information
+ */
 
 namespace Magento2\Gateway;
 
